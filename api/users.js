ccf859108ccb610cf613b578ff87f4dce7328e85
Robostep21 <robostep21@mail.ru>
2026-03-22 18:21:17 +0300

    Create users.js

---
+++ api/users.js
@@ -0,0 +1,15 @@
+// api/users.js
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    const allUsers = [
+    ];
+
+    res.status(200).json(allUsers);
+}
